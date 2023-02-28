4ab6c9d82ca0408e03fd068251c0616fc7d593d1
Pitek69 <pitek220@gmail.com>
2023-02-28 12:54:52 +0100

    dodanie projektu

---
+++ src/components/Emplyees.js
@@ -0,0 +1,22 @@
+const Employees = [
+    {
+        id : "1",
+        Name : 'Leandra',
+        Surname : "Zed",
+        Age : "22"
+    },
+    {
+        Id : "2",
+        Name : "Hovo",
+        Surname : "Neptuno",
+        Age : "33"
+    },
+    {
+        Id : '3',
+        Name : "Dorean",
+        Surname : "Aries",
+        Age : "52"
+    }
+]
+
+export default Employees;
